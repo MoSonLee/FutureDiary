532376ad11d910531da21d790971b5f5edf981b2
이승후 <ron222@naver.com>
2022-09-09 03:04:46 +0900

    first commit

---
+++ FutureDiary/View/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  FutureDiary
+//
+//  Created by 이승후 on 2022/09/09.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setConfigure()
+        setConstraints()
+    }
+    
+    private func setConfigure() {
+        view.backgroundColor = .systemGray
+    }
+    
+    private func setConstraints() {
+        
+    }
+}
+
