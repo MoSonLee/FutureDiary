a81c519122df0c037410119ee220283f05756a07
이승후 <ron222@naver.com>
2022-09-18 02:02:30 +0900

    refactor: code convention

--- FutureDiary/Application/AppDelegate.swift
+++ FutureDiary/Application/AppDelegate.swift
@@ -11,7 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         UNUserNotificationCenter.current().delegate = self
         return true
     }
